796b21321281fcc38cd1ae4c64b5ad8cb1b77cbf
AiU <ai.chan@atherlabs.com>
2025-08-01 15:43:11 +0700

    changed name

--- conf.lua
+++ conf.lua
@@ -8,7 +8,7 @@ function love.conf(t)
     t.version = "11.5"                -- The LÖVE version this game was made for (string)
     t.console = false                  -- Attach a console (boolean, Windows only)
  
-    t.window.title = "BYTEPATH" -- The window title (string)
+    t.window.title = "Ai's Game" -- The window title (string)
     t.window.icon = nil                -- Filepath to an image to use as the window's icon (string)
     t.window.width = gw -- The window width (number)
     t.window.height = gh -- The window height (number)
